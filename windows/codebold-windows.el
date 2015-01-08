222420ccb3206bc11ca33bb02e5f389e3e9447e1
Codebold <info@codebold.de>
2015-01-08 21:17:36 +0100

    Added tramp configuration.

--- windows/codebold-windows.el
+++ windows/codebold-windows.el
@@ -13,5 +13,12 @@
 ;; Start fullscreen
 (w32-send-sys-command 61488)
 
+;; Tramp
+(require 'tramp)
+(add-to-list 'exec-path "C:/Program Files (x86)/PuTTy")
+(setq tramp-default-method "plink")
+;; (setq tramp-default-user "root"
+;;     tramp-default-host "target")
+
 (provide 'codebold-windows)
 ;;; codebold-windows.el ends here
