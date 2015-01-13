78371eba4f5c1f0beb15e3eccbc4e34c13f44773
codebold <mail@codebold.de>
2015-01-13 15:51:56 +0100

    Update server configuration

--- codebold/codebold-server.el
+++ codebold/codebold-server.el
@@ -6,26 +6,15 @@
 
 ;;; Code:
 
-;;
+;; Make C-w end the emacsclient session - Already provided by ergoemacs!
 ;(add-hook 'server-switch-hook
-;          (let ((ergoemacs-is-user-defined-map-change-p t))
-;            (lambda ()
-;              (global-set-key (kbd "C-w") 'server-edit))))
-;              (when (current-local-map)
-;                (use-local-map (copy-keymap (current-local-map))))
-;              (when server-buffer-clients
-;                (local-set-key (kbd "C-w") 'server-edit)))))
-;              (ergoemacs-key "C-w" 'server-edit))))
-                                        ;(global-set-key (kbd "C-w") 'server-edit)
-
-(ergoemacs-theme-component ergoemacs-server ()
-  "Emacs server"
-  (when server-switch-hook
-    (message "### server-switch-hook ###")
-    (global-set-key global-map (kbd "C-w") 'server-edit)))
-
-(ergoemacs-require 'ergoemacs-server)
-
+;          (lambda ()
+;            (when (current-local-map)
+;              (use-local-map (copy-keymap (current-local-map))))
+;            (when server-buffer-clients
+;              (local-set-key (kbd "C-w") 'server-edit))))
 
 (provide 'codebold-server)
+
+
 ;;; codebold-server.el ends here
